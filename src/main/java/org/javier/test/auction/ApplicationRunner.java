58ba5a8ce8f28722bee54362901cf78429516399
javier <javierbada@hotmail.com>
2015-06-19 13:50:40 -0300

    inicial

---
+++ src/main/java/org/javier/test/auction/ApplicationRunner.java
@@ -0,0 +1,38 @@
+package org.javier.test.auction;
+
+public class ApplicationRunner {
+	public static final String SNIPER_ID = "sniper";
+	public static final String SNIPER_PASSWORD = "sniper";
+	protected static final String XMPP_HOSTNAME = null;
+	private static final String STATUS_JOINING = null;
+	private static final String STATUS_LOST = null;
+	private AuctionSniperDriver driver;
+
+	public void startBiddingIn(final FakeAuctionServer auction) {
+		Thread thread = new Thread("Test Application") {
+			@Override
+			public void run() {
+				try {
+					Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD,
+							auction.getItemId());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		thread.setDaemon(true);
+		thread.start();
+		driver = new AuctionSniperDriver(1000);
+		driver.showsSniperStatus(STATUS_JOINING);
+	}
+
+	public void showsSniperHasLostAuction() {
+		driver.showsSniperStatus(STATUS_LOST);
+	}
+
+	public void stop() {
+		if (driver != null) {
+			driver.dispose();
+		}
+	}
+}
---
+++ src/test/java/org/javier/test/auction/SingleMessageListener.java
@@ -0,0 +1,25 @@
+package org.javier.test.auction;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class SingleMessageListener implements MessageListener {
+	private static final TimeUnit SECONDS = null;
+	private final ArrayBlockingQueue<Message> messages = new ArrayBlockingQueue<Message>(1);
+
+	public void processMessage(Chat chat, Message message) {
+		messages.add(message);
+	}
+
+	public void receivesAMessage() throws InterruptedException {
+		assertThat("Message", messages.poll(5, SECONDS), is(notNullValue()));
+	}
+}
