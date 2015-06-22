1259e0f7cd86ad2f5962294cfbff58bbb7c1047f
javier <javierbada@hotmail.com>
2015-06-22 17:27:23 -0300

    rtttyty

--- src/main/java/auctionsniper/ui/MainWindow.java
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -12,6 +12,7 @@ public class MainWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
 	public static final String MAIN_WINDOW_NAME = "Auction Sniper";
 	private static final String STATUS_JOINING = "Joining";
+	public static final String STATUS_LOST = "Lost";
 	private final JLabel sniperStatus = createLabel(STATUS_JOINING);
 	
 	public MainWindow() {
@@ -28,5 +29,9 @@ public class MainWindow extends JFrame {
 		result.setName(Main.SNIPER_STATUS_NAME);
 		result.setBorder(new LineBorder(Color.BLACK));
 		return result;
+	}
+	
+	public void showStatus(String status) {
+		sniperStatus.setText(status);
 	}
 }
--- src/test/java/auctionsniper/FakeAuctionServer.java
+++ src/test/java/auctionsniper/FakeAuctionServer.java
@@ -1,11 +1,20 @@
 package auctionsniper;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
+
 import static java.lang.String.format;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class FakeAuctionServer {
 	private final SingleMessageListener messageListener = new SingleMessageListener();
@@ -48,4 +57,15 @@ public class FakeAuctionServer {
 	public String getItemId() {
 		return itemId;
 	}
-}
+	
+	public class SingleMessageListener implements MessageListener {
+		private final ArrayBlockingQueue<Message> messages = new ArrayBlockingQueue<Message>(1);
+
+		public void processMessage(Chat chat, Message message) {
+			messages.add(message);
+		}
+
+		public void receivesAMessage() throws InterruptedException {
+			assertThat("Message", messages.poll(5, SECONDS), is(notNullValue()));
+		}
+	}}
